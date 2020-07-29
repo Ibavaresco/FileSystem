f22f48470a08488a25f8d9508247a5dc9e0e0acb
Ibavaresco <54424880+Ibavaresco@users.noreply.github.com>
2020-07-29 17:34:32 -0300

    Create Tipos.h

---
+++ Tipos.h
@@ -0,0 +1,38 @@
+//==============================================================================
+// Copyright (c) 2005-2010, Isaac Marino Bavaresco
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//     * Redistributions of source code must retain the above copyright
+//       notice, this list of conditions and the following disclaimer.
+//     * Neither the name of the author nor the
+//       names of its contributors may be used to endorse or promote products
+//       derived from this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE AUTHOR ''AS IS'' AND ANY
+// EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+// DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+// (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+// ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//==============================================================================
+#ifndef		__TIPOS_H__
+#define		__TIPOS_H__
+//==============================================================================
+typedef enum _BOOL { FALSE = 0, TRUE }	BOOL;
+typedef char						bool;
+typedef unsigned char				BYTE;
+typedef unsigned short				WORD;
+typedef unsigned long				DWORD;
+typedef unsigned short long			TWORD;
+typedef unsigned long long			QWORD;
+typedef short long					short_long;
+typedef unsigned char				uchar;
+//==============================================================================
+#endif	//	__TIPOS_H__
+//==============================================================================
